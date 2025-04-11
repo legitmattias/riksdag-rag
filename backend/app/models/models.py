0732c3999cdfc5144b48c54acd9b45546340f9dd
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-04-11 15:15:41 +0200

    Feat: Define Pydantic models for Protocol, Speech, and Clause.

---
+++ backend/app/models/models.py
@@ -0,0 +1,29 @@
+# backend/app/models/models.py
+from pydantic import BaseModel
+from typing import List, Optional
+
+class Clause(BaseModel):
+    number: int
+    title: str
+
+class Protocol(BaseModel):
+    document_id: str
+    title: str
+    parliament_year: str
+    date: str
+    num_clauses: int
+    num_speeches: int
+    clauses: List[Clause]
+
+class Speech(BaseModel):
+    document_id: str
+    hangar_id: str
+    parliament_year: str
+    date: str
+    title: str
+    clause_number: int
+    clause_title: str
+    speech_number: int
+    speaker: str
+    party: Optional[str]
+    text: str
