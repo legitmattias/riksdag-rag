b8c9f983ae6882708cf088196fce4d16760acaf6
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-04-15 11:32:19 +0200

    Feat: Add route to return 204 on missing favicon to remove 404s in log.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,13 @@
 # backend/app/main.py
 from fastapi import FastAPI
 from app.api import meta, data
+from fastapi.responses import Response
 
 app = FastAPI()
 
 app.include_router(meta.router, prefix="/meta")
 app.include_router(data.router, prefix="/data")
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return Response(status_code=204)
