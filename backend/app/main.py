5825e0117dea5dc33e0dd25e9f6b0480cd78a183
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-04-18 15:49:10 +0200

    Chore: Add CORS middleware woth basic options

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,17 @@
 # backend/app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import meta, data
 from fastapi.responses import Response
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(meta.router, prefix="/meta")
 app.include_router(data.router, prefix="/data")
