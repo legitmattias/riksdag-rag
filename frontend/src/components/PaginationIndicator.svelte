30b9b202ea43b8a965a6803bdabd396897adbd9e
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-04-23 15:20:52 +0200

    Feat: Add PaginationIndicator component

---
+++ frontend/src/components/PaginationIndicator.svelte
@@ -0,0 +1,10 @@
+<!-- src/components/PaginationIndicator.svelte -->
+<script lang="ts">
+    export let skip: number = 0;
+    export let limit: number = 25;
+  
+    $: currentPage = Math.floor(skip / limit) + 1;
+  </script>
+  
+  <p class="text-sm text-gray-500 mb-2">Sida {currentPage}</p>
+  
