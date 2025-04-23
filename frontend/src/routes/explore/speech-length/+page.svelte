d62fc4e9885cf2bd1da209b04d9f9ac0dec2a30a
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-04-23 12:13:38 +0200

    Feat: Add Speech length page to explore section

---
+++ frontend/src/routes/explore/speech-length/+page.svelte
@@ -0,0 +1,10 @@
+<!-- src/routes/explore/speech-length/+page.svelte -->
+<script lang="ts">
+    import ChartContainer from '$components/ChartContainer.svelte';
+    import PartySpeechLengthChart from '$components/PartySpeechLengthChart.svelte';
+  </script>
+  
+  <ChartContainer title="Längd på anföranden per parti">
+    <PartySpeechLengthChart />
+  </ChartContainer>
+  
