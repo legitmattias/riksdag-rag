beb61a6438e5fd429820d3ce342ac5f74ffc32b3
Mattias Ubbesen <mu222cu@student.lnu.se>
2025-04-29 15:39:15 +0200

    Style: Reduce size of container

--- frontend/src/components/ChartContainer.svelte
+++ frontend/src/components/ChartContainer.svelte
@@ -2,7 +2,9 @@
 	export let title: string;
 </script>
 
-<div class="rounded bg-white p-4 shadow">
-	<h3 class="mb-2 text-lg font-semibold">{title}</h3>
-	<slot />
+<div class="rounded bg-white p-4 shadow flex flex-col items-center">
+	<h3 class="mb-4 text-lg font-semibold">{title}</h3>
+	<div class="w-full md:w-4/5">
+		<slot />
+	</div>
 </div>
